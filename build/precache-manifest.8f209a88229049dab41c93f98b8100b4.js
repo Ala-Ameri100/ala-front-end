730cc8bdedf97aa20c4e56710e176a36b1192958
vishwanathapoojari <73532652+vishwanathapoojari@users.noreply.github.com>
2020-10-28 11:54:41 +0530

    Add files via upload
    via drag and drop

---
+++ build/precache-manifest.8f209a88229049dab41c93f98b8100b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf520ec408bba469a4b42987354d4731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80c3418c15ba5bfdf68d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89bc80771ad5d30d677b",
+    "url": "/static/js/2.f64cbef6.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.f64cbef6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80c3418c15ba5bfdf68d",
+    "url": "/static/js/main.d5872a87.chunk.js"
+  },
+  {
+    "revision": "8f0764db3d9fa5ee5f11",
+    "url": "/static/js/runtime-main.0ddd29d6.js"
+  }
+]);
